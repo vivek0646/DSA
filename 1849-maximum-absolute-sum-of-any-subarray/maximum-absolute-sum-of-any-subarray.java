3f42fa4fa893f01d0fc90f1b7c97feb4738d4d10
Vivek Gour <vivekgourofficial@gmail.com>
2026-08-12 13:02:33 +0530

    Time: 7 ms (59.11%) | Memory: 66.9 MB (48.78%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -1,17 +1,34 @@
 class Solution {
     public int maxAbsoluteSum(int[] arr) {
+        // int maxend = arr[0];
+        // int minend = arr[0];
+        // int minend2 = Math.abs(arr[0]);   
+        // int ans = Math.abs(arr[0]);
+
+        // for(int i = 1; i< arr.length; i++){
+        //     maxend = Math.max(maxend + arr[i], arr[i]);
+        //     minend = Math.min(minend+arr[i], arr[i]);
+        //     minend2 = Math.max(minend2, Math.abs(minend));
+
+        //     ans = Math.max(ans, Math.max(maxend, minend2));
+        // }
+        // return ans;
+
+        //more polish method by gpt:-
         int maxend = arr[0];
         int minend = arr[0];
-        int minend2 = Math.abs(arr[0]);
-        int ans = Math.abs(arr[0]);
 
-        for(int i = 1; i< arr.length; i++){
+        int maxSum = arr[0];
+        int minSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
             maxend = Math.max(maxend + arr[i], arr[i]);
-            minend = Math.min(minend+arr[i], arr[i]);
-            minend2 = Math.max(minend2, Math.abs(minend));
+            minend = Math.min(minend + arr[i], arr[i]);
 
-            ans = Math.max(ans, Math.max(maxend, minend2));
+            maxSum = Math.max(maxSum, maxend);
+            minSum = Math.min(minSum, minend);
         }
-        return ans;
+
+        return Math.max(maxSum, Math.abs(minSum));
     }
 }
