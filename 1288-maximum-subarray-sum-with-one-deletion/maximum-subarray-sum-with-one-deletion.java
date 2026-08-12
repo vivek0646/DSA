9edd4491b7d5acad26b9652d3879e58ecbaf976b
Vivek Gour <vivekgourofficial@gmail.com>
2026-08-12 12:26:09 +0530

    Time: 7 ms (45.17%) | Memory: 55.8 MB (68.74%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
@@ -1,19 +1,14 @@
 class Solution {
     public int maximumSum(int[] arr) {
         int keep = arr[0];
-        int delete = 0; // Maximum sum with one 
         int ans = arr[0];
-        
-        for (int i = 1; i < arr.length; i++) {
- 
+        int delete = 0;
+
+        for(int i=1; i<arr.length; i++){
             delete = Math.max(delete + arr[i], keep);
-            
-            // Kadane's algorithm
-            keep = Math.max(arr[i], keep + arr[i]);
-    
-            ans = Math.max(ans, Math.max(keep, delete));
+            keep = Math.max(keep + arr[i], arr[i]);
+            ans = Math.max(ans, Math.max(delete, keep));
         }
-        
         return ans;
     }
 }
