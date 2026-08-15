1785f954400272b57406139cb4304f43761173c7
Vivek Gour <vivekgourofficial@gmail.com>
2026-08-15 13:30:58 +0530

    Time: 1 ms (97.59%) | Memory: 47.7 MB (8.75%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,19 +1,18 @@
 class Solution {
     public int pivotIndex(int[] nums) {
- 
-        for(int i =0; i<nums.length; i++){
-        int leftsum = 0;
-        int rightsum = 0;
+        int leftsum =0;
+        int rightsum =0;
+        for(int i=0; i<nums.length; i++){
+            rightsum += nums[i];  //cal the total sum of the array
+        }
 
-            for(int j=0; j<i; j++){
-                leftsum += nums[j];
+        for(int i=0; i<nums.length; i++){
+            rightsum -= nums[i];  //then min the total sum - one by one from st of arr
+
+            if(rightsum == leftsum){
+                return i;               //ans if equal  
             }
-            for(int j = i+1 ; j< nums.length; j++){
-                rightsum += nums[j];
-            }
-            if(leftsum == rightsum){
-                return i;
-            }
+            leftsum += nums[i];         //left will increase;
         }
         return -1;
     }
