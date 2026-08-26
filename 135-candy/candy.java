11d38c73f7c398ab40f1813d4bc8361675cca3df
Vivek Gour <vivekgourofficial@gmail.com>
2026-08-26 12:05:33 +0530

    Time: 3 ms (86.36%) | Memory: 52.3 MB (11.23%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -4,15 +4,14 @@ class Solution {
         Arrays.fill(res, 1)  ;
         for(int i =1; i<ratings.length; i++){
         if(ratings[i] > ratings[i-1]){
-            res[i] = res[i-1] +1 ;
-        }
-        }
+            res[i] = res[i-1] +1 ; //prev wale se extra candy millne chiye, current ma
+        } }
 
     for(int i = ratings.length -2; i>= 0; i--){
         if(ratings[i] > ratings[i+1]){
-            res[i] = Math.max(res[i],res[i+1]+1);
+            res[i] = Math.max(res[i], res[i+1]+1);  //use max fun to avoid the overwrite the smaller value at index, which have bigger value.
         }}
-        
+
         int sum =0;
         for(int j =0; j<res.length; j++){
             sum += res[j];
