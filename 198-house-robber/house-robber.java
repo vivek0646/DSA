c73dad83f3037fa3981c8fb1497eafa5268916e9
Vivek Gour <vivekgourofficial@gmail.com>
2026-09-04 12:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.86%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int dp[];
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return loot(0, nums);
+    }
+    private int loot(int i, int arr[]){
+        if(i >= arr.length) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int pick = arr[i] + loot(i+2, arr);
+        int skip = loot(i+1, arr);
+
+        int ans = Math.max(pick, skip);
+        dp[i] = ans;
+        return ans;
+    }
+}
