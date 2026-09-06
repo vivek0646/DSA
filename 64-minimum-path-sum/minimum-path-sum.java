6c595c1418d43977672708ec4025a5909be036f3
Vivek Gour <vivekgourofficial@gmail.com>
2026-09-07 00:10:46 +0530

    Time: 4 ms (60.27%) | Memory: 50.6 MB (10.49%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,16 +1,25 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length; int n = grid[0].length;
-        int dp[][] = new int[m][n];
+        int m = grid.length;
+        int n = grid[0].length;
 
-        for(int i=0; i<m ; i++){
-            for(int j=0; j<n; j++){
-                if(i==0 && j==0) dp[i][j] = grid[i][j];
-                else if (i==0) dp[i][j] = grid[i][j] + dp[i][j-1]; //1st Row:- fill
-                else if (j==0) dp[i][j] = grid[i][j] + dp[i-1][j]; //1st column:- fill
-                else dp[i][j] = grid[i][j] + Math.min(dp[i][j-1], dp[i-1][j]); // aapna sum + min (upper se Or side se).
+        // Pre-fill the first row
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        // Pre-fill the first column
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        // Fill the rest of the grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += grid[i - 1][j] < grid[i][j - 1] ? grid[i - 1][j] : grid[i][j - 1];
             }
         }
-        return dp[m-1][n-1];
+
+        return grid[m - 1][n - 1];
     }
 }
