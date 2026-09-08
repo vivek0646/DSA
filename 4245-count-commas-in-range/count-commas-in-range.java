c16a136db0e3b97ed58f55f52e576208ae504903
Vivek Gour <vivekgourofficial@gmail.com>
2026-09-08 11:34:14 +0530

    Time: 1 ms (83.65%) | Memory: 42.6 MB (39.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        if(n>999){
-            return n-999;
+        if(n<1000){
+            return 0;
         }
-        else return 0;
+        return n - 999;
     }
 }
